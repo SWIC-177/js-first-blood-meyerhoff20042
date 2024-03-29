5e3a4bc71e440cabed4a238dc6be22e330aed13d
Braden Meyerhoff <braden.meyerhoff@student.swic.edu>
2024-03-28 21:27:55 -0500

    Update index.js (hw wk10 thu2)

--- index.js
+++ index.js
@@ -4,11 +4,31 @@ function verifyAdulthood(person) {
   return `You are not old enough to enter, ${person.name}.`;
 }
 
-const person = {
-  name: "John",
-  age: 178,
-};
+const people = [
+  {
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
 
-const adultMessage = verifyAdulthood(person);
+for (let i = 0; i < people.length; i += 1) {
+  const adultMessage = verifyAdulthood(people[i]);
 
-console.log(adultMessage);
+  console.log(adultMessage);
+}
